d0c3f45a4f739ab5b8cfff3dc9aa1c9683286974
sebpearce <sebpearce@gmail.com>
2015-10-20 21:46:49 +0900

    code overhaul for better structure & style; css embellishments

--- main.js
+++ main.js
@@ -1,237 +1,164 @@
+'use strict';
+
 /*
  * main.js
  * 
  * New Age Bullshit Generator
- * © 2014 Seb Pearce (sebpearce.com)
+ * © 2014-15 Seb Pearce (sebpearce.com)
  * Licensed under the MIT License.
  * 
  */
 
-// deepCopy function taken from:
-// http://james.padolsey.com/javascript/deep-copying-of-objects-and-arrays/
-function deepCopy(obj) {
-    if (Object.prototype.toString.call(obj) === '[object Array]') {
-        var out = [], i = 0, len = obj.length;
-        for ( ; i < len; i++ ) {
-            out[i] = arguments.callee(obj[i]);
-        }
-        return out;
+// Toolkit of useful functions
+var kit = {
+
+  copyArrayOfArrays(arr) {
+    var result = [];
+    for (var i = 0; i < arr.length; i++) {
+      result[i] = arr[i].slice();
     }
-    if (typeof obj === 'object') {
-        var out = {}, i;
-        for ( i in obj ) {
-            out[i] = arguments.callee(obj[i]);
-        }
-        return out;
+    return result;
+  },
+
+  capitalizeFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  },
+
+  randomInt(max) {
+    return Math.floor(Math.random() * (max + 1));
+  },
+
+};
+
+// The generator in all its quantum glory
+var bs = {
+
+  sentencePool: [],
+
+  initializeSentencePool() {
+    this.sentencePool = [];
+    this.sentencePool = kit.copyArrayOfArrays(this.sentencePatterns);
+  },
+
+  removeSentenceFromPool(topic, el) {
+    if (el > -1) {
+      this.sentencePool[topic].splice(el, 1);
     }
-    return obj;
-}
+  },
 
-function removeSentence(topic, element) {
+  retrieveRandomWordOfType(type) {
+    var rand = kit.randomInt(this.bullshitWords[type].length - 1);
+    return this.bullshitWords[type][rand];
+  },
 
-  if (element > -1) {
-    sentencePatternsClone[topic].splice(element, 1);
-  }
+  generateSentence(topic) {
 
-}
+    var patternNumber = kit.randomInt(this.sentencePool[topic].length - 1);
+    var pattern = this.sentencePool[topic][patternNumber];
 
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-function randomInt(max) {
-    return Math.floor(Math.random()*(max+1));
-}
-
-
-function retrieveRandomWordOfType(type) {
-
-  // find how long the array of words is for given type
-  var max = type.length - 1;
-
-  // get a random number to represent a word in the array
-  var rand = randomInt(max);
-
-  var result = type[rand];
-
-  // type.splice(rand,1); 
-  // console.log(type);
-
-  return result;
-
-}
-
-function generateSentence(topic) {
-
-  var length = sentencePatternsClone[topic].length;
-
-  // generate random sentence pattern
-  var patternNumber = randomInt(length - 1);
-
-  // console.log('sentencePatternsClone[' + topic + '].length = ' + (patternNumber+1));
-
-  var pattern = sentencePatternsClone[topic][patternNumber];
-
-
-  if (typeof pattern == 'undefined') {
-    console.log('ran out.');
-    return ":( ";
-  }
-
-  var pattern = pattern.replace(/([\.,;\?])/g,' $1');
-  var pattern = pattern.split(' ');
-
-  // remove the pattern from the sentence array so it isn't re-used
-  removeSentence(topic, patternNumber);
-
-  // console.log('sentencePatternsClone.length is now ' + sentencePatternsClone.length);
-  if (sentencePatternsClone[topic].length == 0) {
-    sentencePatternsClone.splice(topic, 1);
-    // console.log('topic removed!');
-    // console.log('sentencePatternsClone.length is now ' + sentencePatternsClone.length);
-  }
-
-  var result = "";
-
-  for (var x in pattern) {
-
-    switch (pattern[x]) {
-      case 'nCosmos': result += retrieveRandomWordOfType(nCosmos);
-      break;
-      case 'nPerson': result += retrieveRandomWordOfType(nPerson);
-      break;
-      case 'nPersonPlural': result += retrieveRandomWordOfType(nPersonPlural);
-      break;
-      case 'nMass': result += retrieveRandomWordOfType(nMass);
-      break;
-      case 'nMassBad': result += retrieveRandomWordOfType(nMassBad);
-      break;
-      case 'nPath': result += retrieveRandomWordOfType(nPath);
-      break;
-      case 'nOurPlural': result += retrieveRandomWordOfType(nOurPlural);
-      break;
-      case 'nOf': result += retrieveRandomWordOfType(nOf);
-      break;
-      case 'ing': result += retrieveRandomWordOfType(ing);
-      break;
-      case 'adj': result += retrieveRandomWordOfType(adj);
-      break;
-      case 'adjBig': result += retrieveRandomWordOfType(adjBig);
-      break;
-      case 'adjWith': result += retrieveRandomWordOfType(adjWith);
-      break;
-      case 'adjPrefix': result += retrieveRandomWordOfType(adjPrefix);
-      break;
-      case 'vtMass': result += retrieveRandomWordOfType(vtMass);
-      break;
-      case 'vtPerson': result += retrieveRandomWordOfType(vtPerson);
-      break;
-      case 'vtDestroy': result += retrieveRandomWordOfType(vtDestroy);
-      break;
-      case 'viPerson': result += retrieveRandomWordOfType(viPerson);
-      break;
-      case 'nTheXOf': result += retrieveRandomWordOfType(nTheXOf);
-      break;
-      case 'ppPerson': result += retrieveRandomWordOfType(ppPerson);
-      break;
-      case 'ppThingPrep': result += retrieveRandomWordOfType(ppThingPrep);
-      break;
-      case 'fixedAdvP': result += retrieveRandomWordOfType(fixedAdvP);
-      break;
-      case 'fixedAdvPPlace': result += retrieveRandomWordOfType(fixedAdvPPlace);
-      break;
-      case 'fixedNP': result += retrieveRandomWordOfType(fixedNP);
-      break;      
-      case 'nSubject': result += retrieveRandomWordOfType(nSubject);
-      break;
-      case 'vOpenUp': result += retrieveRandomWordOfType(vOpenUp);
-      break;      
-      case 'vTraverse': result += retrieveRandomWordOfType(vTraverse);
-      break;
-      default: result += pattern[x];
+    if (typeof pattern == 'undefined') {
+      console.log('ran out of pattern ' + patternNumber);
     }
 
-    result += ' ';
-  }
+    // insert a space before . , ; ? so we can split the string into an array
+    var pattern = pattern.replace(/([\.,;\?])/g,' $1');
+    var pattern = pattern.split(' ');
 
-  result = result.trim();
-  result = capitalizeFirstLetter(result);
+    // remove the pattern from the sentence pool so it can't be re-used
+    this.removeSentenceFromPool(topic, patternNumber);
 
-  if (result.charAt(result.length-1) != '?') {
-    result += '. ';
-  } else {
-    result += ' ';
-  }
-
-  // remove spaces before commas/periods/semicolons
-  result = result.replace(/ ([,\.;\?])/g,'$1');
-
-  return result;
-}
-
-function generateText(numberOfSentences, sentenceTopic) {
-
-  var fullText = "";
-
-  for (var i = 0; i < numberOfSentences; i++) {
-
-    fullText += generateSentence(sentenceTopic);
-
-    // in case the topic got deleted
-    if (typeof sentencePatternsClone[sentenceTopic] == 'undefined') {
-      sentenceTopic = randomInt(sentencePatternsClone.length - 1);
-      // console.log('topic reset to ' + sentenceTopic);
+    // remove the topic from the sentence pool if there are no sentences left
+    // for that particular topic
+    if (this.sentencePool[topic].length === 0) {
+      this.sentencePool.splice(topic, 1);
     }
 
-  } 
+    var result = '';
+    for (var x in pattern) {
+      // if word matches one of the placeholder words (e.g. nPerson),
+      // replace it with a random instance of its type (e.g. warrior)
+      if (this.bullshitWords.hasOwnProperty(pattern[x])) {
+        result += this.retrieveRandomWordOfType(pattern[x]);
+      } else {
+        result += pattern[x];
+      }
+      result += ' ';
+    }
 
-  // replace 'a [vowel]' with 'an [vowel]'
-  // I added a \W before the [Aa] because one time I got
-  // "Dogman is the antithesis of knowledge" :)
-  fullText = fullText.replace(/(^|\W)([Aa]) ([aeiou])/g,'$1$2n $3');
+    // replace 'a [vowel]' with 'an [vowel]'
+    // I added a \W before the [Aa] because one time I got
+    // 'Dogman is the antithesis of knowledge' :)
+    result = result.replace(/(^|\W)([Aa]) ([aeiou])/g,'$1$2n $3');
 
-  // take care of prefixes (delete the space after the hyphen)
-  fullText = fullText.replace(/- /g,'-');
+    result = result.trim();
+    result = kit.capitalizeFirstLetter(result);
 
-  return fullText;
+    // remove spaces before commas/periods/semicolons
+    result = result.replace(/ ([,\.;\?])/g,'$1');
+    // take care of prefixes (delete the space after the hyphen)
+    result = result.replace(/- /g,'-');
+    // add space after question marks if they're mid-sentence
+    result = result.replace(/\?(\w)/g,'? $1');
 
-}
+    return result;
+  },
 
-$('.topbar button').click(function(){
+  generateText(numberOfSentences, sentenceTopic) {
 
-  sentencePatternsClone = deepCopy(sentencePatterns);
+    var fullText = '';
+    for (var i = 0; i < numberOfSentences; i++) {
+      fullText += this.generateSentence(sentenceTopic);
+      // if the topic has been deleted, pick another topic
+      if (typeof this.sentencePool[sentenceTopic] == 'undefined') {
+        sentenceTopic = kit.randomInt(this.sentencePool.length - 1);
+      }
+    } 
 
-  // generate random topic
-  var sentenceTopic = 0;
+    // insert a space between sentences (after periods and question marks)
+    fullText = fullText.replace(/([\.\?])(\w)/g,'$1 $2');
 
-  $('h1').text(generateText(1, sentenceTopic));
+    return fullText;
+  },
 
-  $('h2').text(generateText(2, sentenceTopic));
+};
 
-  sentenceTopic = randomInt(sentencePatternsClone.length - 2);
-
-  $('h3').text(generateText(1, sentenceTopic));
-
-  $('p').each(function( i ) {
-    sentenceTopic = randomInt(sentencePatternsClone.length - 1);
-    $(this).text(generateText(3, sentenceTopic));
-  });
-
-  sentenceTopic = randomInt(sentencePatternsClone.length - 1);
-
-  $('blockquote').text(generateText(1, sentenceTopic));
-
-  // change image
-  $('img').attr('src', 'http://placeimg.com/640/480/nature?' + 
-    Math.floor(Math.random() * 100));
-
+$(' .topbar button ').hover(function(){
+  $(this).removeClass('glowjump');
 });
 
 
-$(document).ready(function() {
+// Page interaction
+$(' .topbar button ').click(function(){
+
+  $(' .page-flash ').show(1, function(){
+
+    // generate random topic
+    var sentenceTopic = 0;
+
+    $(' #main-heading ').text(bs.generateText(1, sentenceTopic));
+    $(' #sub-heading ').text(bs.generateText(2, sentenceTopic));
+    sentenceTopic = kit.randomInt(bs.sentencePool.length - 2);
+    $(' #third-heading ').text(bs.generateText(1, sentenceTopic));
+    $(' p ').each(function(i) {
+      sentenceTopic = kit.randomInt(bs.sentencePool.length - 1);
+      $(this).text(bs.generateText(3, sentenceTopic));
+    });
+    sentenceTopic = kit.randomInt(bs.sentencePool.length - 1);
+    $(' #quote ').text(bs.generateText(1, sentenceTopic));
+
+    // change image
+    $(' #nature-image ').fadeTo(1, 0, function(){
+      $(this).attr('src', 'http://placeimg.com/640/480/nature?' + 
+      Math.floor(Math.random() * 100)).bind('onreadystatechange load', function(){
+        if (this.complete) $(this).fadeTo(1000, 1);
+      });
+    });
+
+  }).fadeOut(1000);
+
+  bs.initializeSentencePool();
 
 });
-
 
 
 
--- patterns.js
+++ patterns.js
@@ -3,115 +3,124 @@
  * A list of sentence patterns to be parsed by main.js
  * 
  * New Age Bullshit Generator
- * © 2014 Seb Pearce (sebpearce.com)
+ * © 2014-15 Seb Pearce (sebpearce.com)
  * Licensed under the MIT License.
  * 
  */
 
-var sentencePatterns = new Array();
+bs.sentencePatterns = [
 
-// explaining
-sentencePatterns[0] = [
-  'nMass is the driver of nMass',
-  'nMass is the nTheXOf of nMass, and of us',
-  'You and I are nPersonPlural of the nCosmos',
-  'We exist as fixedNP',
-  'We viPerson, we viPerson, we are reborn',
-  'Nothing is impossible',
-  'This life is nothing short of a ing nOf of adj nMass',
-  'Consciousness consists of fixedNP of quantum energy. “Quantum” means a ing of the adj',
-  'The goal of fixedNP is to plant the seeds of nMass rather than nMassBad',
-  'nMass is a constant',
-  'By ing, we viPerson',
-  'The nCosmos is adjWith fixedNP',
-  'To vTraverse the nPath is to become one with it',
-  'Today, science tells us that the essence of nature is nMass',
-  'nMass requires exploration'
-];
+  // explaining
+  [
+    'nMass is the driver of nMass.',
+    'nMass is the nTheXOf of nMass, and of us.',
+    'You and I are nPersonPlural of the nCosmos.',
+    'We exist as fixedNP.',
+    'We viPerson, we viPerson, we are reborn.',
+    'Nothing is impossible.',
+    'This life is nothing short of a ing nOf of adj nMass.',
+    'Consciousness consists of fixedNP of quantum energy. “Quantum” means a ing of the adj.',
+    'The goal of fixedNP is to plant the seeds of nMass rather than nMassBad.',
+    'nMass is a constant.',
+    'By ing, we viPerson.',
+    'The nCosmos is adjWith fixedNP.',
+    'To vTraverse the nPath is to become one with it.',
+    'Today, science tells us that the essence of nature is nMass.',
+    'nMass requires exploration.',
+  ],
 
-// warnings
-sentencePatterns[1] = [
-  'We can no longer afford to live with nMassBad',
-  'Without nMass, one cannot viPerson',
-  'Only a nPerson of the nCosmos may vtMass this nOf of nMass',
-  'You must take a stand against nMassBad',
-  'Yes, it is possible to vtDestroy the things that can vtDestroy us, but not without nMass on our side',
-  'nMassBad is the antithesis of nMass',
-  'You may be ruled by nMassBad without realizing it. Do not let it vtDestroy the nTheXOf of your nPath',
-  'the complexity of the present time seems to demand a ing of our nOurPlural if we are going to survive',
-  'nMassBad is born in the gap where nMass has been excluded',
-  'Where there is nMassBad, nMass cannot thrive'
-];
+  // warnings
+  [
+    'We can no longer afford to live with nMassBad.',
+    'Without nMass, one cannot viPerson.',
+    'Only a nPerson of the nCosmos may vtMass this nOf of nMass.',
+    'You must take a stand against nMassBad.',
+    'Yes, it is possible to vtDestroy the things that can vtDestroy us, but not without nMass on our side.',
+    'nMassBad is the antithesis of nMass.',
+    'You may be ruled by nMassBad without realizing it. Do not let it vtDestroy the nTheXOf of your nPath.',
+    'the complexity of the present time seems to demand a ing of our nOurPlural if we are going to survive.',
+    'nMassBad is born in the gap where nMass has been excluded.',
+    'Where there is nMassBad, nMass cannot thrive.',
+  ],
 
-// future hope
-sentencePatterns[2] = [
-  'Soon there will be a ing of nMass the likes of which the nCosmos has never seen',
-  'It is time to take nMass to the next level',
-  'Imagine a ing of what could be',
-  'Eons from now, we nPersonPlural will viPerson like never before as we are ppPerson by the nCosmos',
-  'It is a sign of things to come',
-  'The future will be a adj ing of nMass',
-  'This nPath never ends',
-  'We must learn how to lead adj lives in the face of nMassBad',
-  'We must vtPerson ourselves and vtPerson others',
-  'The nOf of nMass is now happening worldwide',
-  'We are being called to explore the nCosmos itself as an interface between nMass and nMass',
-  'It is in ing that we are ppPerson',
-  'The nCosmos is approaching a tipping point'
-];
+  // future hope
+  [
+    'Soon there will be a ing of nMass the likes of which the nCosmos has never seen.',
+    'It is time to take nMass to the next level.',
+    'Imagine a ing of what could be.',
+    'Eons from now, we nPersonPlural will viPerson like never before as we are ppPerson by the nCosmos.',
+    'It is a sign of things to come.',
+    'The future will be a adj ing of nMass.',
+    'This nPath never ends.',
+    'We must learn how to lead adj lives in the face of nMassBad.',
+    'We must vtPerson ourselves and vtPerson others.',
+    'The nOf of nMass is now happening worldwide.',
+    'We are being called to explore the nCosmos itself as an interface between nMass and nMass.',
+    'It is in ing that we are ppPerson.',
+    'The nCosmos is approaching a tipping point.',
+  ],
 
-// you and your problems
-sentencePatterns[3] = [
-  'Although you may not realize it, you are adj',
-  'nPerson, look within and vtPerson yourself',
-  'Have you found your nPath?',
-  'How should you navigate this adj nCosmos?',
-  'It can be difficult to know where to begin',
-  'If you have never experienced this nOf fixedAdvP, it can be difficult to viPerson',
-  'The nCosmos is calling to you via fixedNP. Can you hear it?'
-];
+  // you and your problems
+  [
+    'Although you may not realize it, you are adj.',
+    'nPerson, look within and vtPerson yourself.',
+    'Have you found your nPath?',
+    'How should you navigate this adj nCosmos?',
+    'It can be difficult to know where to begin.',
+    'If you have never experienced this nOf fixedAdvP, it can be difficult to viPerson.',
+    'The nCosmos is calling to you via fixedNP. Can you hear it?',
+  ],
 
-// history
-sentencePatterns[4] = [
-  'Throughout history, humans have been interacting with the nCosmos via fixedNP',
-  'Reality has always been adjWith nPersonPlural whose nOurPlural are ppThingPrep nMass',
-  'Our conversations with other nPersonPlural have led to a ing of adjPrefix adj consciousness',
-  'Humankind has nothing to lose',
-  'We are in the midst of a adj ing of nMass that will vOpenUp the nCosmos itself',
-  'Who are we? Where on the great nPath will we be ppPerson?',
-  'We are at a crossroads of nMass and nMassBad'
-  // 'Even nProphet '
-];
+  // history
+  [
+    'Throughout history, humans have been interacting with the nCosmos via fixedNP.',
+    'Reality has always been adjWith nPersonPlural whose nOurPlural are ppThingPrep nMass.',
+    'Our conversations with other nPersonPlural have led to a ing of adjPrefix adj consciousness.',
+    'Humankind has nothing to lose.',
+    'We are in the midst of a adj ing of nMass that will vOpenUp the nCosmos itself.',
+    'Who are we?Where on the great nPath will we be ppPerson?',
+    'We are at a crossroads of nMass and nMassBad.',
+  ],
 
-// selling point
-sentencePatterns[5] = [
-  'Through nSubject, our nOurPlural are ppThingPrep nMass',
-  'nSubject may be the solution to what’s holding you back from a adjBig nOf of nMass',
-  'You will soon be ppPerson by a power deep within yourself — a power that is adj, adj',
-  'As you viPerson, you will enter into infinite nMass that transcends understanding'
+  // selling point
+  [
+    'Through nSubject, our nOurPlural are ppThingPrep nMass.',
+    'nSubject may be the solution to what’s holding you back from a adjBig nOf of nMass.',
+    'You will soon be ppPerson by a power deep within yourself — a power that is adj, adj.',
+    'As you viPerson, you will enter into infinite nMass that transcends understanding.',
+  ],
+
 ];
 
 /*
-nCosmos
-nPerson
-nPersonPlural
-nOurPlural
-nMass
-nMassBad
-nPath
-nOf
-ing
-adj
-adjBig
-adjWidth
-adjPrefix
-vtMass
-vtPerson
-viPerson
-nTheXOf
-ppPerson
-ppThingPrep
-fixedAdvP
-fixedNP
-nSubject
+
+Available word types:
+
+- adj
+- adjBig
+- adjPrefix
+- adjWith
+- fixedAdvP
+- fixedAdvPPlace
+- fixedNP
+- ing
+- nCosmos
+- nMass
+- nMassBad
+- nOf
+- nOurPlural
+- nPath
+- nPerson
+- nPersonPlural
+- nSubject
+- nTheXOf
+- ppPerson
+- ppThingPrep
+- vOpenUp
+- vTraverse
+- viPerson
+- vtDestroy
+- vtMass
+- vtPerson
+
  */
