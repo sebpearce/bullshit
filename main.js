82dccf96e52e9ebbf9389478ea63ada7b21a66e1
Seb Pearce <sebpearce@gmail.com>
2022-11-30 21:02:56 +1100

    Use vanilla JS instead of jQuery

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
  * main.js
  *
  * New Age Bullshit Generator
- * © 2014-15 Seb Pearce (sebpearce.com)
+ * © 2014–2022 Seb Pearce (sebpearce.com)
  * Licensed under the MIT License.
  *
  * TODO:
@@ -16,63 +16,53 @@
  * not pull patterns out of use.
  */
 
-// Toolkit of useful functions
-var kit = {
-  copyArrayOfArrays: function (arr) {
-    var result = []
-    for (var i = 0; i < arr.length; i++) {
-      result[i] = arr[i].slice()
-    }
-    return result
-  },
+function capitalizeFirstLetter(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
 
-  capitalizeFirstLetter: function (str) {
-    return str.charAt(0).toUpperCase() + str.slice(1)
-  },
+function randomInt(max) {
+  return Math.floor(Math.random() * (max + 1))
+}
 
-  randomInt: function (max) {
-    return Math.floor(Math.random() * (max + 1))
-  },
+function replaceAWithAn(sentence) {
+  // replace 'a [vowel]' with 'an [vowel]'
+  // I added a \W before the [Aa] because one time I got
+  // 'Dogman is the antithesis of knowledge' :)
+  return sentence.replace(/(^|\W)([Aa]) ([aeiou])/g, "$1$2n $3")
+}
 
-  replaceAWithAn: function (sentence) {
-    // replace 'a [vowel]' with 'an [vowel]'
-    // I added a \W before the [Aa] because one time I got
-    // 'Dogman is the antithesis of knowledge' :)
-    return sentence.replace(/(^|\W)([Aa]) ([aeiou])/g, "$1$2n $3")
-  },
+function removeSpacesBeforePunctuation(sentence) {
+  // remove spaces before commas/periods/semicolons
+  return sentence.replace(/ ([,\.;\?])/g, "$1")
+}
 
-  removeSpacesBeforePunctuation: function (sentence) {
-    // remove spaces before commas/periods/semicolons
-    return sentence.replace(/ ([,\.;\?])/g, "$1")
-  },
+function deleteSpaceAfterHyphen(sentence) {
+  // take care of prefixes (delete the space after the hyphen)
+  return sentence.replace(/- /g, "-")
+}
 
-  deleteSpaceAfterHyphen: function (sentence) {
-    // take care of prefixes (delete the space after the hyphen)
-    return sentence.replace(/- /g, "-")
-  },
+function addSpaceAfterQuestionMarks(sentence) {
+  // add space after question marks if they're mid-sentence
+  return sentence.replace(/\?(\w)/g, "? $1")
+}
 
-  addSpaceAfterQuestionMarks: function (sentence) {
-    // add space after question marks if they're mid-sentence
-    return sentence.replace(/\?(\w)/g, "? $1")
-  },
+function insertSpaceBeforePunctuation(sentence) {
+  return sentence.replace(/([\.,;\?])/g, " $1")
+}
 
-  insertSpaceBeforePunctuation: function (sentence) {
-    return sentence.replace(/([\.,;\?])/g, " $1")
-  },
-
-  insertSpaceBetweenSentences: function (text) {
-    // insert a space between sentences (after periods and question marks)
-    return text.replace(/([\.\?])(\w)/g, "$1 $2")
-  },
+function insertSpaceBetweenSentences(text) {
+  // insert a space between sentences (after periods and question marks)
+  return text.replace(/([\.\?])(\w)/g, "$1 $2")
 }
 
 // The generator in all its quantum glory
-var bs = {
+const bs = {
   sentencePool: [],
 
   initializeSentencePool: function () {
-    this.sentencePool = []
-    this.sentencePool = kit.copyArrayOfArrays(this.sentencePatterns)
+    // [...foo] only does shallow copies
+    // but sentencePatterns is an array of arrays
+    this.sentencePool = [...this.sentencePatterns.map((group) => group.slice())]
   },
 
   removeSentenceFromPool: function (topic, el) {
@@ -82,33 +72,31 @@ var bs = {
   },
 
   retrieveRandomWordOfType: function (type) {
-    var rand = kit.randomInt(this.bullshitWords[type].length - 1)
+    const rand = randomInt(this.bullshitWords[type].length - 1)
     return this.bullshitWords[type][rand]
   },
 
   cleanSentence: function (sentence) {
-    var result
-
-    result = kit.replaceAWithAn(sentence)
+    let result = replaceAWithAn(sentence)
     result = result.trim()
-    result = kit.capitalizeFirstLetter(result)
-    result = kit.removeSpacesBeforePunctuation(result)
-    result = kit.deleteSpaceAfterHyphen(result)
-    result = kit.addSpaceAfterQuestionMarks(result)
+    result = capitalizeFirstLetter(result)
+    result = removeSpacesBeforePunctuation(result)
+    result = deleteSpaceAfterHyphen(result)
+    result = addSpaceAfterQuestionMarks(result)
 
     return result
   },
 
   generateSentence: function (topic) {
-    var patternNumber = kit.randomInt(this.sentencePool[topic].length - 1)
-    var pattern = this.sentencePool[topic][patternNumber]
+    const patternNumber = randomInt(this.sentencePool[topic].length - 1)
+    let pattern = this.sentencePool[topic][patternNumber]
 
-    if (typeof pattern == "undefined") {
-      console.log("ran out of pattern " + patternNumber)
+    if (typeof pattern === "undefined") {
+      throw new Error("ran out of pattern " + patternNumber)
     }
 
     // insert a space before . , ; ? so we can split the string into an array
-    pattern = kit.insertSpaceBeforePunctuation(pattern)
+    pattern = insertSpaceBeforePunctuation(pattern)
     pattern = pattern.split(" ")
 
     // remove the pattern from the sentence pool so it can't be re-used
@@ -120,8 +108,8 @@ var bs = {
       this.sentencePool.splice(topic, 1)
     }
 
-    var result = ""
-    for (var x in pattern) {
+    let result = ""
+    for (let x in pattern) {
       // if word matches one of the placeholder words (e.g. nPerson),
       // replace it with a random instance of its type (e.g. warrior)
       if (this.bullshitWords.hasOwnProperty(pattern[x])) {
@@ -137,60 +125,80 @@ var bs = {
     return result
   },
 
-  generateText: function (numberOfSentences, sentenceTopic) {
-    var fullText = ""
-    for (var i = 0; i < numberOfSentences; i++) {
-      fullText += this.generateSentence(sentenceTopic)
+  generateText: function ({ numberOfSentences, topicIndex }) {
+    let fullText = ""
+    for (let i = 0; i < numberOfSentences; i++) {
+      fullText += this.generateSentence(topicIndex)
       // if the topic has been deleted, pick another topic
-      if (typeof this.sentencePool[sentenceTopic] == "undefined") {
-        sentenceTopic = kit.randomInt(this.sentencePool.length - 1)
+      if (typeof this.sentencePool[topicIndex] === "undefined") {
+        topicIndex = randomInt(this.sentencePool.length - 1)
       }
     }
 
-    fullText = kit.insertSpaceBetweenSentences(fullText)
+    fullText = insertSpaceBetweenSentences(fullText)
 
     return fullText
   },
 }
 
-$(" .topbar button ").hover(function () {
-  $(this).removeClass("glowjump")
-})
+const reionizeBtn = document.querySelector(".reionize")
+const pageFlash = document.querySelector(".page-flash")
+const mainHeading = document.querySelector("#main-heading")
+const subHeading = document.querySelector("#sub-heading")
+const thirdHeading = document.querySelector("#third-heading")
+const quote = document.querySelector(".quote")
+const natureImage = document.querySelector(".nature-image")
+const paragraphs = document.querySelectorAll(".bs-paragraph")
 
-// Page interaction
-$(" .topbar button ").click(function () {
-  $(" .page-flash ")
-    .show(1, function () {
-      // generate random topic
-      var sentenceTopic = 0
-
-      $(" #main-heading ").text(bs.generateText(1, sentenceTopic))
-      $(" #sub-heading ").text(bs.generateText(2, sentenceTopic))
-      sentenceTopic = kit.randomInt(bs.sentencePool.length - 2)
-      $(" #third-heading ").text(bs.generateText(1, sentenceTopic))
-      $(" .bs-paragraph ").each(function (i) {
-        sentenceTopic = kit.randomInt(bs.sentencePool.length - 1)
-        $(this).text(bs.generateText(3, sentenceTopic))
-      })
-      sentenceTopic = kit.randomInt(bs.sentencePool.length - 1)
-      $(" #quote ").text(bs.generateText(1, sentenceTopic))
-
-      // change image
-      $(" #nature-image ").fadeTo(1, 0, function () {
-        $(this)
-          .attr(
-            "src",
-            "https://placeimg.com/640/480/nature?" +
-              Math.floor(Math.random() * 100)
-          )
-          .bind("onreadystatechange load", function () {
-            if (this.complete) $(this).fadeTo(1000, 1)
-          })
-      })
-
-      $("#nabg-bot-paragraph").hide()
-    })
-    .fadeOut(1000)
+function reionizeElectrons() {
+  pageFlash.classList.replace("hide", "show")
+  window.setTimeout(() => {
+    pageFlash.classList.replace("show", "hide")
+  }, 20)
 
   bs.initializeSentencePool()
-})
+
+  // generate random topic
+  let topicIndex = 0
+
+  mainHeading.textContent = bs.generateText({
+    numberOfSentences: 1,
+    topicIndex,
+  })
+  subHeading.textContent = bs.generateText({
+    numberOfSentences: 2,
+    topicIndex,
+  })
+
+  topicIndex = randomInt(bs.sentencePool.length - 2)
+
+  thirdHeading.textContent = bs.generateText({
+    numberOfSentences: 1,
+    topicIndex,
+  })
+
+  paragraphs.forEach((p) => {
+    topicIndex = randomInt(bs.sentencePool.length - 1)
+    p.textContent = bs.generateText({ numberOfSentences: 3, topicIndex })
+  })
+
+  topicIndex = randomInt(bs.sentencePool.length - 1)
+
+  quote.textContent = bs.generateText({ numberOfSentences: 1, topicIndex })
+
+  natureImage.classList.replace("show", "hide")
+
+  const imageUrl =
+    "https://placeimg.com/640/480/nature?" + Math.floor(Math.random() * 100)
+
+  natureImage.setAttribute("src", imageUrl)
+}
+
+reionizeBtn.onclick = reionizeElectrons
+
+natureImage.onload = () => natureImage.classList.replace("hide", "show")
+
+// cancel the button jump if user hovers on it mid-jump
+reionizeBtn.addEventListener("mouseenter", () =>
+  reionizeBtn.classList.remove("glowjump")
+)
